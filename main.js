b3e21327d2976d714ece2edb549d5a554aa32a67
AndreaSdr70 <andrea@sdringola.it>
2024-07-20 19:07:29 +0200

    Selfwork-Array-1

---
+++ main.js
@@ -0,0 +1,15 @@
+// Ordine decrescente
+let array1 = [3,7,-2,5,8,1,2,5,6,-4,12,25];
+
+array1.sort((a,b) => b - a);
+
+console.log(array1);
+
+
+
+// Ordine crescente
+let array2 = [3,7,-2,5,8,1,2,5,6,-4,12,25];
+
+array2.sort((a,b) => a - b);
+
+console.log(array2);
